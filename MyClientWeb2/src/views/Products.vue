eef515c228c755ce15eba4ebad0d902ad0cbef04
Mel Leeb <mleeb@bennettadelson.com>
2019-07-26 07:22:11 -0400

    Products axios work

--- MyClientWeb2/src/views/Products.vue
+++ MyClientWeb2/src/views/Products.vue
@@ -5,20 +5,31 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       appName: 'Products page',
       error: '',
-      isBusy: false
+      isBusy: false,
+      products: []
     }
   },
   methods: {
+    getProducts: function () {
+      this.error = ''
+      this.isBusy = true
+      axios.get('/api/products')
+        .then((response) => {
+          this.isBusy = false
+          this.products = response.data.value
+        }, (error) => {
+          this.isBusy = false
+          this.error = error
+        })
+    }
   },
   mounted () {
-    // TODO: Call the API
-    this.error = ''
-    this.isBusy = true
   },
   computed: {
   }
