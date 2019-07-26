e0b8f2c961df69e37fd65fb33112a1bbbadae47e
Mel Leeb <mleeb@bennettadelson.com>
2019-07-26 07:13:07 -0400

    starter code for Products page

--- MyClientWeb2/src/views/Products.vue
+++ MyClientWeb2/src/views/Products.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="products">
-    <h1>This is a Products page</h1>
+    <h1>This is a {{ appName }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      appName: 'Products page',
+      error: '',
+      isBusy: false
+    }
+  },
+  methods: {
+  },
+  mounted () {
+    // TODO: Call the API
+    this.error = ''
+    this.isBusy = true
+  },
+  computed: {
+  }
+}
+</script>
